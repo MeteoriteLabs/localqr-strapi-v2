350fc71ee5e416b2d19089ca4bcc53f05386ef1d
Meteorite Labs <tandavkrishna.sg@gmail.com>
2025-09-26 12:07:18 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
-  host: env('PUBLIC_URL', 'api.localqr.earth'),
-  // port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://api.localqr.earth'),
   app: {
     keys: env.array('APP_KEYS'),
   },
