5715c8ef0534b7304680f3ec7a1ee49287dca0e5
Ubuntu <ubuntu@ip-10-0-12-128.ap-south-1.compute.internal>
2025-10-03 00:00:03 +0000

    Automated commit on 2025-10-03 00:00:03

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://13.126.235.177:${port}'),
+  url: env('PUBLIC_URL', 'http://13.126.235.177:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
