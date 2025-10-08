0695ed30a10fb2f50ab653071ed5d790b3463652
Meteorite Labs <tandavkrishna.sg@gmail.com>
2025-10-08 14:51:21 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,8 +2,10 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'http://13.126.235.177:1337'),
-  proxy: { koa: env.bool('IS_PROXIED', true) },
-  app: {
+  proxy: {
+    koa: true
+  },
+  app: 
     keys: env.array('APP_KEYS'),
   },
 });
