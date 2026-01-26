94cb63897d2022c2248cbbe300a878d10a1d40a5
Ubuntu <tandavkrishna.sh@gmail.com>
2026-01-26 12:07:14 +0000

    Automated commit on 2026-01-26 12:07:14

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://13.126.235.177:1337'),
+  url: env('PUBLIC_URL', 'http://77.42.95.255:1337'),
   proxy: { koa: env.bool('IS_PROXIED', true) },
   app: {
     keys: env.array('APP_KEYS'),
